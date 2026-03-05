5f7a485de7c3ea42ee22d69b660c1814f5228732
Patrik Luiz <patrikluiz2004@hotmail.com>
2026-03-05 08:26:27 -0300

    Switch - case
    Switch - case é similar ao if else, entretanto é baseado em casos, ou seja, em resultados específicos, então em caso de hoje fazer sol, será uma ocorrência, em caso hoje chover, é outra ocorrência e por ai vai.

---
+++ escolha(switch&case).js
@@ -0,0 +1,22 @@
+const nota = 4
+
+switch(nota){
+    case 10:
+    case 9:
+        console.log('MB')
+        break
+    case 8:
+    case 7:
+        console.log('B')
+        break
+    case 6:
+    case 5:
+        console.log('R')
+        break
+    case 4:
+    case 3:
+    case 2:
+    case 1:
+    case 0:
+        console.log('I')
+}
