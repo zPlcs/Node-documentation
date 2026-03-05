322c9332d2f8a3d0de29bb39a9e69fdfc8554049
Patrik Luiz <patrikluiz2004@hotmail.com>
2026-03-05 07:36:07 -0300

    Create condicionais.js

---
+++ condicionais.js
@@ -0,0 +1,21 @@
+const idade = 20
+const crianca = 10
+const adolescente = 17
+const idoso = 60
+
+if (idade >= 18){
+    console.log('Você é maior de idade.')
+}
+else {
+    console.log('Você é menor de idade')
+}
+
+if(idade <= crianca){
+    console.log('Você é uma criança')
+}
+else if (idade <= adolescente){
+    console.log('Você é um adolescente')
+}
+else {
+    console.log('Você é um idoso')
+}
