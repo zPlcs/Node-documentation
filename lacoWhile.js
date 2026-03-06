9fba36725dd0d1cfe280ce5178836d83aec0bf02
Patrik Luiz <patrikluiz2004@hotmail.com>
2026-03-06 08:15:09 -0300

    Loop com While e do While
    Fazemos um loop rodar ENQUANTO uma condição for verdadeira e podemos também executar uma função e depois verificar se ela for verdadeira, em caso de true, ela continua o loop, se não, ela somente executa uma vez.

---
+++ lacoWhile.js
@@ -0,0 +1,23 @@
+let cont = 1
+let cont2 = 0
+//Enquanto (cont menor ou igual a 10, executar)
+while(cont<=10){
+    console.log(cont)
+    cont++
+}
+
+const cond = true
+
+//Fazer (código) enquanto (condição)
+do {
+    console.log('Contador 2: ', cont2)
+    cont2++
+} while (cont2 <= 10)
+
+const palavra = "Patrik"
+let i = 0
+
+while(i < palavra.length){
+    console.log(palavra[i])
+    i++
+}
