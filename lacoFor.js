0ab0c31d6fcd4b645166551ef44b3a96b55742e4
Patrik Luiz <patrikluiz2004@hotmail.com>
2026-03-06 07:58:16 -0300

    Loop com For
    Criamos uma repetição dentro de um espaço (range) para repetir alguma função ou comando.

---
+++ lacoFor.js
@@ -0,0 +1,14 @@
+const idade = 21
+const palavra = 'Patrik'
+
+//Para (var i;enquanto i menor ou igual a idade;incrementa 1 em i)
+for(let i = 0;i<=idade;i++){
+    console.log(i)
+}
+
+
+//Contador de caracteres
+for(let cont = 1; cont < palavra.length; cont++){
+    console.log(palavra[cont])
+}
+
