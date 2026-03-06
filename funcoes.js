e9607b3ed7a12ba55e87d080a9f0bd65b55fe44d
Patrik Luiz <patrikluiz2004@hotmail.com>
2026-03-06 15:22:43 -0300

    Funções
    Declaramos uma serie de comandos, literalmente uma função com ou sem retorno de dados para o código executar.

---
+++ funcoes.js
@@ -0,0 +1,8 @@
+//Função ->  nome da função(x da função) -> executação da função -> retorno/resultado da função(se tiver) 
+//'nome' é um parametro para a função
+function HelloWorld(nome){
+    return console.log('Olá', nome)
+}
+
+//Patrik é um argumento/contexto para o parametro da função
+HelloWorld('Patrik')
