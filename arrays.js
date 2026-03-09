83323a263972bb81a4983db4ce564a734b804bdd
Patrik Luiz <patrikluiz2004@hotmail.com>
2026-03-09 08:29:26 -0300

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -9,3 +9,22 @@ console.log(frutas[0])
 console.log(frutas.length)
 frutas.push('Melão')
 console.log(frutas[frutas.length-1])
+
+//Para retirar itens de um array, selecionamos o indice e quantos indices após o indice escolhido, será removido
+frutas.splice(2,1)
+
+console.log('\nUsando for: ')
+for(let i = 0;i < frutas.length; i++){
+    console.log('Indice: ',i,'\nNome: ',frutas[i])
+}
+
+
+console.log('\nUsando forEach:')
+frutas.forEach((valor, indice) => {
+    console.log('Indice: ',indice,'\nValor: ',valor)
+})
+
+console.log('\nUsando for of:')
+for(const fruta of frutas){
+    console.log('Valor: ',fruta)
+}
