ce26028220a13435df4c61653cea3dab86fd7a8d
Patrik Luiz <patrikluiz2004@hotmail.com>
2026-03-09 08:19:57 -0300

    Arrays
    Arrays são formas de dados guardadas em variáveis no modelo de lista, seguindo o modelo .json

---
+++ arrays.js
@@ -0,0 +1,11 @@
+const frutas = [
+    'Banana',
+    'Melancia',
+    'Morango',
+    'Maracuja'
+]
+
+console.log(frutas[0])
+console.log(frutas.length)
+frutas.push('Melão')
+console.log(frutas[frutas.length-1])
