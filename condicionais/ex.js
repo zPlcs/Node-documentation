890a814a1dbe912484b97f46bb26716032801256
Patrik Luiz <patrikluiz2004@hotmail.com>
2026-03-13 09:09:25 -0300

    Update ex.js

--- condicionais/ex.js
+++ condicionais/ex.js
@@ -1,49 +1,49 @@
-// const salario = 9050
+// Você está desenvolvendo um sistema de controle de acesso para um evento. Para entrar, a pessoa precisa ter 18 anos ou mais.
 
-// if(salario < 3999){
-//     console.log(salario*0.09)
-// } else if (salario >= 4000 && salario < 6999){
-//     console.log(salario*0.07)
-// } else if (salario >= 7000 && salario <10999){
-//     console.log(salario*0.05)
-// } else if (salario >= 11000) {
-//     console.log(salario*0.03)
+// Crie um programa que, dado o valor da idade, verifique se a pessoa pode ou não entrar no evento. 
+// Exiba a mensagem informando se o acesso foi ou não permitido.
+
+// const idade = 19
+
+// if ((idade < 18) && idade > 0){
+//     console.log('Acesso não permitido')
 // } else {
-//     console.log('Salario invalido')
+//     console.log('Acesso negado')
 // }
 
-// const ano = 2024
+// Você está desenvolvendo um sistema meteorológico para informar sobre o clima do dia. Com base na temperatura, o 
+// sistema deve classificar o dia conforme os critérios abaixo:
 
-// if(((ano%4 === 0 && ano%100!== 0)) || (ano%100 === 0 && ano%400 === 0)){
-//     console.log(`${ano} é bissexto`)
+// Menor que 15: "Frio"
+// De 15 a 25: "Agradável"
+// Acima de 25: "Quente"
+// Escreva um programa que, dado um valor de temperatura e imprima a mensagem correspondente.
+
+// const temperatura = 26;
+
+// if(temperatura < 15){
+//     console.log('Frio')
+// } else if(temperatura >= 15 && temperatura <= 25){
+//     console.log('Agradável')
 // } else {
-//     console.log(`${ano} não é bissexto`)
+//     console.log('Quente')
 // }
 
-// const estudante = 'Patrik'
-// const n1 = 4
-// const f1 = 1
+// Você é uma pessoa desenvolvedora de um sistema para uma escola que organiza atividades para diferentes faixas etárias. As regras são:
 
-// const bonusEstudantil = (n1 >= 8) && (f1 <= 2) 
-//     ?   console.log(`Bonús garantido para ${estudante}`)
-//     :   console.log('Aluno com nota ou presença insuficiente')
+// Se a pessoa tiver menos de 12 anos, ela deve ser classificada como "Infantil".
+// Se a pessoa tiver 12 anos ou mais, mas menos de 18 anos, ela deve ser classificada como "Juvenil".
+// Se a pessoa tiver 18 anos ou mais, ela deve ser classificada como "Adulto".
+// Crie um programa que, dado o valor da idade, classifique a pessoa na categoria correspondente.
 
-const usuario = {
-    nome: 'Patrik',
-    lvDeAcesso: 0
+// const idade = 18;
 
-}
-
-switch (usuario.lvDeAcesso) {
-    case 1:
-        console.log('Usuário Free: Acesso limitado')
-        break
-    case 2:
-        console.log('Usuario Premium: Tem acesso a todas as funções')
-        break
-    case 3:
-        console.log('Usuário Ultimate: Tem acesso a todas as funções e bônus especiais')
-        break
-    default:
-        console.log('Nivel de acesso inválido')
-} 
+// if(idade < 12){
+//     console.log('Infantil')
+// } else if (idade >= 12 && idade < 18){
+//     console.log('Juvenil')
+// } else if (idade >= 18){
+//     console.log('Adulto')
+// } else {
+//     console.log('Idade inválida')
+// }
